8812a5fc1d007e1454e635567b1d766cd94bade4
Logan <pagelerlogan@gmail.com>
2021-11-02 21:05:08 -0700

    update

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ page = """
                 <input type="text" name="units" value = {}> 
                 <button name="reset" value="reset"> Reset Accuracy </button>
                 <p> Units avaible: 4-11 </p>
-                <br><br>
             </div>
             <<>>
         </form> 
@@ -35,7 +34,7 @@ page = """
 def generate(prev_ans, accuracy, button, values):
     count = random.randrange(0, len(values))
 
-    question = random.randrange(0, 1)
+    question = random.randrange(0, 2)
     ans = 0
     if question == 1:
         question = 1 + random.randrange(0, 3)
@@ -53,26 +52,28 @@ def generate(prev_ans, accuracy, button, values):
         arr[1] = int(arr[1]) + 1
         if prev_ans[4] == prev_ans[button]:
             arr[0] = int(arr[0]) + 1
-            text += "<p> Correct! </p>"
+            text += '<p class="correct"> Correct! </p>'
         else:
-            text += "<p> Incorrect! </p>"
+            text += '<p class="incorrect"> Incorrect! </p>'
 
-        text = f"<p> Previous Answer: {prev_ans[4]}  Accuracy: {arr[0]}/{arr[1]} </p>"
+        text = (
+            text
+            + f"\n<p> Previous Answer: {prev_ans[4]}  Accuracy: {arr[0]}/{arr[1]} </p>"
+        )
 
     text = (
         f"""
             <div class="button-panel">
-                <p> {values[count][question]}
-                
+                <p class="question"> {values[count][question]}</p>
                 <div>
-                    <button name="button 1" value="1"> {answers[0]} </button>
-                    <button name="button 2" value="2"> {answers[1]} </button>
+                    <button name="button 1" value="1" class="answer-button"> {answers[0]} </button>
+                    <button name="button 2" value="2" class="answer-button"> {answers[1]} </button>
                 </div>
                 <div>
-                    <button name="button 3" value="3"> {answers[2]} </button>
-                    <button name="button 4" value="4"> {answers[3]} </button>
+                    <button name="button 3" value="3" class="answer-button"> {answers[2]} </button>
+                    <button name="button 4" value="4" class="answer-button"> {answers[3]} </button>
                 </div>
-                <input name="prev" type="hidden" value="{answers+[values[count][ans], f"{arr[0]}/{arr[1]}"]}"> 
+                <input class="answer" name="prev" type="hidden" value="{answers+[values[count][ans], f"{arr[0]}/{arr[1]}"]}"> 
             </div>
             """
         + text
@@ -83,12 +84,12 @@ def generate(prev_ans, accuracy, button, values):
 
 lessons = []
 for i in range(4, 12):
-    print(i)
     lessons += [
-        line[:-1].split(",")
-        for line in open(f"kanji/lesson{i}.txt", "r", encoding="utf-8").readlines()
+        [
+            line[:-1].split(",")
+            for line in open(f"kanji/lesson{i}.txt", "r", encoding="utf-8").readlines()
+        ]
     ]
-print(lessons)
 
 
 @app.route("/", methods=["POST"])
@@ -102,11 +103,13 @@ def post_something():
         try:
             if "-" in r:
                 temp = r.split("-")
-                values += lessons[int(r[0]) : int(r[1])]
+                values += [
+                    j for i in lessons[int(temp[0]) - 4 : int(temp[1]) - 4] for j in i
+                ]
             else:
-                values += lessons[int(r)]
+                values += lessons[int(r) - 4]
         except:
-            values = lessons
+            values = [j for i in lessons for j in i]
     if request.form.get("button 1"):
         prev_ans = [txt[1:-1] for txt in request.form.get("prev")[1:-1].split(", ")]
         new = page.replace(
@@ -145,7 +148,7 @@ def post_something():
 def index():
     new = page.replace(
         "<<>>",
-        generate(None, "0/0", None, lessons),
+        generate(None, "0/0", None, [j for i in lessons for j in i]),
     ).format(css, "4-11")
     return new
 
